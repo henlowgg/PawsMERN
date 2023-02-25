ea06928b272f05b648291f9522f614b3dedce1db
Jack Ault <henlowgg@outlook.com>
2023-02-25 12:17:14 -0500

    models setup

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGS */
 const __filename = fileURLToPath(import.meta.url);
@@ -36,7 +37,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /* Routes with files */
-app.post()
+app.post("/auth/register", upload.single("picture"), register);
 
 
 /* Mongoose Setup */
---
+++ server/models/User.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+// keeping password simple for now, will change later to more secure method
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 50,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 50,
+        },
+        email: {
+            type: String,
+            required: true,
+            max: 50,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 5,
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 5,
+        },
+    }
+)
